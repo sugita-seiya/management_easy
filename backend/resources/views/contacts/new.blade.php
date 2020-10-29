1e7b07666b4243d60c475787768a2c708818c505
sugita-seiya <satreu16@ezweb.ne.jp>
2020-10-30 06:58:16 +0900

    連絡事項登録機能の実装

--- backend/resources/views/contacts/new.blade.php
+++ backend/resources/views/contacts/new.blade.php
@@ -5,24 +5,25 @@
   <table class="table table-bordered my-5">
     <tbody>
       <tr>
-        <td class="pr-5">日付</td>
+        <td class="pr-5" style="background-color:#e9e1de;">日付</td>
         <td>{{$year}}年{{$month}}月{{$day}}日({{$week}})</td>
         {{Form::hidden('year',$year)}}
         {{Form::hidden('month',$month)}}
         {{Form::hidden('day',$day)}}
       </tr>
       <tr>
-        <td class="pr-5">件名</td>
+        <td class="pr-5" style="background-color:#e9e1de;">件名</td>
         <td>{{ Form::text('subject', null) }}</td>
       </tr>
       <tr>
-        <td class="pr-5">本文</td>
+        <td class="pr-5" style="background-color:#e9e1de;">本文</td>
         <td>
         {{Form::textarea('body', null, ['rows' => 10,'cols' => 70])}}
         </td>
       </tr>
     </tbody>
   </table>
+
   <div class="text-center">
     <a href={{ route('contact.index') }}>
       <button type="button" class="btn btn-light">戻る</button>

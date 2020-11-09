f047e60acc502919ba50dc40ea04e7584f274c06
sugita-seiya <satreu16@ezweb.ne.jp>
2020-11-10 05:57:35 +0900

    出勤管理テーブル作成

--- backend/database/migrations/2020_11_10_050039_create_works_table.php
+++ backend/database/migrations/2020_11_10_050039_create_works_table.php
@@ -15,7 +15,28 @@ class CreateWorksTable extends Migration
     {
         Schema::create('works', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->integer('year')->nullable(false);
+            $table->integer('month')->nullable(false);
+            $table->integer('day')->nullable(false);
+            $table->time('workstart');
+            $table->time('workend');
+            $table->time('breaktime');
+            $table->time('total_worktime');
+            $table->string('remark');
+            $table->integer('approval_flg');
+            $table->unsignedBigInteger('work_section_id');
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
+
+            $table->foreign('user_id')  //追加user_idに外部キー制約をつける。usersテーブルのidカラムを参照してそのカラムがなくなったらカスケード的に処理する。
+            ->references('id')
+            ->on('users')
+            ->onDelete('cascade');
+
+            $table->foreign('work_section_id')
+            ->references('id')
+            ->on('work_sections')
+            ->onDelete('cascade');
         });
     }
 

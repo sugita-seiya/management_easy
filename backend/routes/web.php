166a09427c58d22d97cc966267f964c15665ef1c
sugita-seiya <satreu16@ezweb.ne.jp>
2021-01-01 07:30:45 +0900

    必要なコメントの追加

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -12,13 +12,13 @@ Auth::routes();
 Route::group(['middleware' => ['auth']], function () {
     #一般社員ページ
     Route::resource('contact', 'ContactController');
-    Route::resource('work', 'WorkController')->only(['index','edit','update','show']);
-    Route::post('work', 'WorkController@workrequest')->name('work.request');
-    Route::post('work/{work}', 'WorkController@store')->name('work.store');
+    Route::resource('work', 'WorkController')            ->only(['index','edit','update','show']);
+    Route::post('work', 'WorkController@workrequest')    ->name('work.request');
+    Route::post('work/{work}', 'WorkController@store')   ->name('work.store');
     Route::resource('worksystem', 'WorksystemController')->only(['index','edit','update']);
     #管理者専用ページ
-    Route::get('approvel', 'Work_approvelController@index')->name('user_approvel.index')->middleware('check_approvel');
-    Route::get('userall', 'Work_approvelController@userindex')->name('user_all.index')->middleware('check_approvel');
-    Route::get('approvel/{id}', 'Work_approvelController@wrokindex')->name('work_approvel.index')->middleware('check_approvel');
-    Route::post('approvel/{id}', 'Work_approvelController@update')->name('work_approvel.update')->middleware('check_approvel');
+    Route::get('approvel', 'Work_approvelController@index')         ->name('user_approvel.index') ->middleware('check_approvel');
+    Route::get('userall', 'Work_approvelController@userindex')      ->name('user_all.index')      ->middleware('check_approvel');
+    Route::get('approvel/{id}', 'Work_approvelController@wrokindex')->name('work_approvel.index') ->middleware('check_approvel');
+    Route::post('approvel/{id}', 'Work_approvelController@update')  ->name('work_approvel.update')->middleware('check_approvel');
 });

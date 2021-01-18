1ff26f40b4111dba9c02fcd186bbb0d24d0965e9
sugita-seiya <satreu16@ezweb.ne.jp>
2021-01-19 06:17:12 +0900

    管理者用コントローラのリファクタリング

--- backend/app/Http/Controllers/Work_approvelController.php
+++ backend/app/Http/Controllers/Work_approvelController.php
@@ -15,15 +15,16 @@ class Work_approvelController extends Controller
 {
     public function __construct()
     {
-        $this->year    = date("Y");                #当年を取得(yyyy)
-        $this->month   = date("m");                #当月を取得(m)
+        $this->year    = date("Y");      #当年を取得(yyyy)
+        $this->month   = date("m");      #当月を取得(m)
+        $this->work    = new work;       #勤怠クラスのインスタンス
+        $this->user    = new User;       #ユーザークラスのインスタンス
     }
 
     public function index()
     {
         #勤怠を申請and承認したユーザーレコードを取得
-        $work           = new work;
-        $approving_user = $work->Works_Approvel($this->year,$this->month);
+        $approving_user = $this->work->Works_Approvel($this->year,$this->month);
         //レコード取得出来なかった場合、nullをセット
         if (count($approving_user) == 0) {
             $approving_user = null;
@@ -46,16 +47,14 @@ class Work_approvelController extends Controller
     public function wrokindex($user_id)
     {
         //勤怠申請したユーザーidを受け取り、当月の勤怠一覧を取得する
-        $work      = new work;
-        $work_list = $work->Work_Edit($user_id,$this->year,$this->month);
+        $work_list = $this->work->Work_Edit($user_id,$this->year,$this->month);
         if (count($work_list) == 0) {
             $errer_messege = "レコード取得に失敗しました。管理者にご連絡ください。";
             return view('layouts.errer', ['errer_messege' => $errer_messege]);
         }
 
         //勤怠申請したユーザーレコードを取得する
-        $user      = new User;
-        $user_list = $user->User_All($user_id);
+        $user_list = $this->user->User_All($user_id);
         if (count($user_list) == 0) {
             $errer_messege = "レコード取得に失敗しました。管理者にご連絡ください。";
             return view('layouts.errer', ['errer_messege' => $errer_messege]);
@@ -66,7 +65,7 @@ class Work_approvelController extends Controller
         return view('work_approvel.workindex',[
             'work_list' => $work_list,
             'user_list' => $user_list,
-            'work'      => $work
+            'work'      => $this->work
         ]);
     }
 
@@ -74,8 +73,7 @@ class Work_approvelController extends Controller
     {
         //管理者が承認or差し戻し時にworkテーブルのapprovel_flgを更新する
         $approval_flg   = request('approval_flg');
-        $work           = new work;
-        $results        = $work->Approvel_Update($user_id,$approval_flg,$this->year,$this->month);
+        $results        = $this->work->Approvel_Update($user_id,$approval_flg,$this->year,$this->month);
         if ($results != true){
             $errer_messege = "レコード取得に失敗しました。管理者にご連絡ください。";
             return view('layouts.errer', ['errer_messege' => $errer_messege]);

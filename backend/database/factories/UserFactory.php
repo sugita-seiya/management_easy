55872a8745415bbc3a10b0ab6daba7f14e7e1d71
sugita-seiya <satreu16@ezweb.ne.jp>
2021-01-22 05:48:57 +0900

    連絡事項コントローラのtest実施

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -33,13 +33,13 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'f_name' => $faker->name,
-        'r_name' => $faker->name,
+        'f_name' => 'sugita',
+        'r_name' => 'seiya',
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
         'work_system_id' => '3',
-        'authorty_id' => '1',
+        'authorty_id' => '2',
     ];
 });
--- backend/tests/Feature/Contact/ContactControllerTest.php
+++ backend/tests/Feature/Contact/ContactControllerTest.php
@@ -9,8 +9,9 @@ namespace Tests\Feature\Contact;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-use App\Contact;                        #contactクラスの宣言
-use App\User;                           #contactクラスの宣言
+use App\Contact;                        #連絡事項クラスの宣言
+use App\User;                           #ユーザークラスの宣言
+use App\Work;                           #勤怠クラスの宣言
 use DB;                                 #DBクラスの宣言
 
 class ContactControllerTest extends TestCase
@@ -22,66 +23,223 @@ class ContactControllerTest extends TestCase
      * @return void
      */
 
-    # DBからデータを取得してindexページへ遷移出来ること
     public function testindex()
     {
-        # ログインユーザー作成
-        $user = factory(User::class)->create();
-        // $user = factory(User::class)->create([
-        //     'password' => bcrypt('aaaaaaaa')
-        // ]);
+        #1-1.データ取得確認(データ有)
+        $user = factory(User::class)->create();                # ログインユーザー作成
 
-        # 連絡事項を全て取得出来ていること
-        $contacts   = Contact::all();
-        $this->assertNotEmpty($contacts);
+        #1-2.ログインユーザー情報取得
+        $contacts   = Contact::all();                          # 10件取得
+        $this->assertEquals(10, count($contacts));
 
-        # ログイン状態で任意ページへアクセスチェック
-        $response = $this->actingAs($user)->get('/contact');
+        #1-3.ページ遷移確認(連絡事項一覧)
+        $response = $this->actingAs($user)->get('/contact');   # ログイン状態で任意ページへアクセスチェック
         $this->assertEquals(200, $response->status());
+    }
+
+    public function testindex_notdata()
+    {
+        #1-4.データ取得確認(データ無)
+        DB::table('contacts')->delete();
+        $contacts   = Contact::all();                       # 0件
+        if (count($contacts) == 0){
+            $contacts = 'null';
+        }
+        $this->assertEquals('null', $contacts);             #$cocntactsにnullが取得出来る事
     }
 
     public function testcreate()
     {
-        # ログインユーザー作成
-        $user = factory(User::class)->create();
-        // $user = factory(User::class)->create([
-        //     'password' => bcrypt('aaaaaaaa')
-        // ]);
+        #2-1.ログインユーザー情報取得
+        $user                   = factory(User::class)->create();                  # ログインユーザー作成
+        $login_user_id          = $user->id;
+        $user_new               = new User;
+        $authortyid_information = $user_new->Authortyid_Get($login_user_id);
+        $this->assertEquals(2, $authortyid_information['login_user_authortyid']);  # ログインユーザーの権限フラグ=2
+        $this->assertEquals(1, $authortyid_information['admin_user']);             # 管理者用フラグ=1
+        $this->assertEquals(2, $authortyid_information['general_user']);           # 一般者用フラグ=2
 
-        # ログイン状態で任意ページへアクセスチェック
-        $response = $this->actingAs($user)->get('/contact/create');
+        #2-2.ページ遷移確認(連絡事項登録)
+        $response = $this->actingAs($user)->get('/contact/create');                # ログイン状態でアクセスチェック
+        $this->assertEquals(200, $response->status());
+    }
+
+    public function teststore()
+    {
+        #3-1.連絡事項のテーブル登録確認
+        $user             = factory(User::class)->create();           # ログインユーザー作成
+        $contact          = new Contact;
+        $contact->year    = 2021;
+        $contact->month   = 1;
+        $contact->day     = 25;
+        $contact->subject = 'TEST';
+        $contact->body    = 'TEST';
+        $contact->user_id = $user->id;
+        $results          = $contact->save();
+        $this->assertEquals(true, $results);                          # DB登録結果がtrue
+
+        #3-2.ログインユーザー名取得確認
+        $user_new         = new User;
+        $login_user_id    = $user->id;
+        $login_user_name  = $user_new->UserName_Get($login_user_id);  #ログインユーザー名取得
+        $login_fname      = $login_user_name->f_name;
+        $login_rname      = $login_user_name->r_name;
+        $this->assertEquals('sugita', $login_fname);                  # 姓 = sugita
+        $this->assertEquals('seiya', $login_rname);                   # 名 = seiya
+
+        #3-3.slack通知確認
+        $work_new         = new Work;
+        $url              = config('app.SLACK_WEBHOOK_URL');          #slackのURL
+        $channel          = config('app.SLACK_CHANNEL');              #slackのチャンネル名
+        $icon             = config('app.FACEICON');                   #アイコン
+        $send_result      = $work_new->send_slack($url,$channel,$icon,$login_fname,$login_rname,$contact->body);
+        $this->assertEquals('ok',$send_result);                       # 送信結果=ok
+
+        #3-4.ページ遷移確認(連絡事項一覧)
+        $response         = $this->actingAs($user)->get('/contact');  # ログイン状態で任意ページへアクセスチェック
+        $this->assertEquals(200, $response->status());
+    }
+
+    public function testshow()
+    {
+        #4-1.連絡事項編集ページのID取得確認
+        $user             = factory(User::class)->create();           # ログインユーザー作成
+        $login_user_id    = $user->id;
+        $contact          = new Contact;
+        $contact->id      = 1;
+        $contact->year    = 2021;
+        $contact->month   = 1;
+        $contact->day     = 25;
+        $contact->subject = 'TEST';
+        $contact->body    = 'TEST';
+        $contact->user_id = $login_user_id;
+        $contact->save();
+        $contact_id       = DB::table('contacts')
+                            ->select('id')
+                            ->where('id', 1)
+                            ->get();
+        $this->assertEquals(1,$contact_id[0]->id);                       # ID = 1
+
+
+
+        #4-2.ログインユーザー情報取得
+        $user_new               = new User;
+        $authortyid_information = $user_new->Authortyid_Get($login_user_id);
+        $this->assertEquals(2, $authortyid_information['login_user_authortyid']);  # ログインユーザーの権限フラグ=2
+        $this->assertEquals(1, $authortyid_information['admin_user']);             # 管理者用フラグ=1
+        $this->assertEquals(2, $authortyid_information['general_user']);           # 一般者用フラグ=2
+
+        #4-3.ページ遷移確認(連絡事項詳細)
+        $response         = $this->actingAs($user)->get('/contact/$contact_id[0]->id');  # ログイン状態で任意ページへアクセスチェック
+        $this->assertEquals(200, $response->status());
+    }
+
+    public function testedit()
+    {
+        #5-1.連絡事項編集ページのID取得確認
+        $user             = factory(User::class)->create();           # ログインユーザー作成
+        $login_user_id    = $user->id;
+        $contact          = new Contact;
+        $contact->id      = 2;
+        $contact->year    = 2021;
+        $contact->month   = 1;
+        $contact->day     = 25;
+        $contact->subject = 'TEST';
+        $contact->body    = 'TEST';
+        $contact->user_id = $login_user_id;
+        $contact->save();
+        $contact_id       = DB::table('contacts')
+                            ->select('id')
+                            ->where('id', 2)
+                            ->get();
+        $this->assertEquals(2,$contact_id[0]->id);                       # ID = 2
+
+        #5-2.ログインユーザー情報取得
+        $user_new               = new User;
+        $authortyid_information = $user_new->Authortyid_Get($login_user_id);
+        $this->assertEquals(2, $authortyid_information['login_user_authortyid']);  # ログインユーザーの権限フラグ=2
+        $this->assertEquals(1, $authortyid_information['admin_user']);             # 管理者用フラグ=1
+        $this->assertEquals(2, $authortyid_information['general_user']);           # 一般者用フラグ=2
+
+        #5-3.ページ遷移確認(連絡事項編集)
+        $response         = $this->actingAs($user)->get('/contact/$contact_id[0]->id/edit');  # ログイン状態で任意ページへアクセスチェック
         $this->assertEquals(200, $response->status());
     }
 
     public function testupdate()
     {
-        # 1.連絡事項更新レコードID取得確認
-        $contact   = DB::table('contacts')->where('id','1')->first();
-        $this->assertEquals(1, $contact->id);                           #ID=1である事
+        # 6-1.連絡事項更新レコードID取得確認
+        $user             = factory(User::class)->create();           # ログインユーザー作成
+        $login_user_id    = $user->id;
+        $contact          = new Contact;
+        $contact->id      = 3;
+        $contact->year    = 2021;
+        $contact->month   = 1;
+        $contact->day     = 25;
+        $contact->subject = 'TEST';
+        $contact->body    = 'TEST';
+        $contact->user_id = $login_user_id;
+        $contact->save();
 
-        # 2. 連絡事項更新レコード更新確認
+        $contact_id       = DB::table('contacts')
+                            ->select('id')
+                            ->where('id', 3)
+                            ->get();
+
+        $this->assertEquals(3,$contact_id[0]->id);                           #ID=3
+
+        # 6-2. 連絡事項更新レコード更新確認
         $results = DB::table('contacts')
-                    ->where('id', $contact->id)
+                    ->where('id', $contact_id[0]->id)
                     ->update([
-                        'year'    => '2021',
-                        'month'   => '1',
-                        'day'     => '25',
+                        'year'    => 2021,
+                        'month'   => 1,
+                        'day'     => 30,
                         'subject' => 'TEST1',
                         'body'    => 'TEST1',
                     ]);
-        // $this->assertEquals(1, $results);                               #更新レコード数が1である事
-        $contact   = DB::table('contacts')->where('id','1')->first();
-        $this->assertEquals('2021', $contact->year);                    #更新されていることを確認
-        $this->assertEquals('1', $contact->month);                      #更新されていることを確認
-        $this->assertEquals('25', $contact->day);                       #更新されていることを確認
-        $this->assertEquals('TEST1', $contact->subject);                #更新されていることを確認
-        $this->assertEquals('TEST1', $contact->body);                   #更新されていることを確認
+        $contact = DB::table('contacts')
+                        ->select('*')
+                        ->where('id', 3)
+                        ->get();
+        $this->assertEquals(2021, $contact[0]->year);                    #更新されていることを確認
+        $this->assertEquals(1, $contact[0]->month);                      #更新されていることを確認
+        $this->assertEquals(30, $contact[0]->day);                       #更新されていることを確認
+        $this->assertEquals('TEST1', $contact[0]->subject);                #更新されていることを確認
+        $this->assertEquals('TEST1', $contact[0]->body);                   #更新されていることを確認
 
-        # 3.ページ遷移確認(連絡事項一覧)
-        $user = factory(User::class)->create([
-            'password' => bcrypt('aaaaaaaa')
-        ]);
+        # 6-3.ページ遷移確認(連絡事項一覧)
         $response = $this->actingAs($user)->get(route('work.index'));
         $response->assertStatus(200);
+    }
+
+    public function testdestroy()
+    {
+        # 7-1.連絡事項更新レコードID取得確認
+        $user             = factory(User::class)->create();           # ログインユーザー作成
+        $login_user_id    = $user->id;
+        $contact          = new Contact;
+        $contact->id      = 4;
+        $contact->year    = 2021;
+        $contact->month   = 1;
+        $contact->day     = 25;
+        $contact->subject = 'TEST';
+        $contact->body    = 'TEST';
+        $contact->user_id = $login_user_id;
+        $contact->save();
+
+        $contact_id       = DB::table('contacts')
+                            ->select('*')
+                            ->where('id', 4)
+                            ->first();
+
+        $this->assertEquals(4,$contact_id->id);                    #ID=4
+
+        # 7-2. 連絡事項更新レコード削除確認
+        $results    = DB::table('contacts')
+                        ->where('id', $contact_id->id)
+                        ->delete();
+
+        $this->assertEquals(true, $results);                          # DB登録結果がtrue
     }
 }
